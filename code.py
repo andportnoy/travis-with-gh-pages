53d482c671647710140a4664c5692a5dec1ff3e3
Andrey Portnoy <aportnoy@ucsd.edu>
2018-07-14 21:40:39 -0700

    initial

---
+++ code.py
@@ -0,0 +1,20 @@
+def func(arg1, arg2):
+    """This is an example documented function.
+    
+    Parameters
+    ----------
+    arg1 : whatever
+        Just an argument
+    arg2 : also whatever
+        Just an argument
+
+        
+    Returns
+    -------
+    sum : whatever
+        sum of arg1 and arg2
+        
+ 
+    """
+
+    return arg1 + arg2
